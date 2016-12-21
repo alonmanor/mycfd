bd1724c122074f66ccfc255b1546cb6c17828a60
alon <alon@manor>
2016-12-21 17:02:50 +0200

    eigenvector subroutine added

--- mymath.f90
+++ mymath.f90
@@ -28,6 +28,43 @@ subroutine sort3(x1,x2,x3)
 			call swap(x2,x3)
 		endif
        END SUBROUTINE sort3
+ 
+ subroutine det(a11,a12,a13,a21,a22,a23,a31,a32,a33,d)
+! return a 3x3 determinant in d
+		USE decomp_2d
+		real(mytype), intent(in) :: a11,a12,a13,a21,a22,a23,a31,a32,a33
+		real(mytype), intent(out) :: d
+		
+		d = a11*(a22*a33-a23*a32) - a12*(a21*a33-a23*a31) + a13*(a21*a32-a22*a31)
+ end subroutine det
+ 
+ subroutine eigvec(G11,G12,G13,G22,G23,G33,x1,x2,x3,v)
+ ! given the symmetric 3X3 matrix Gij and it's eigenvalues xi
+ ! return the eigenvectors in the columns of v
+		USE decomp_2d
+       IMPLICIT NONE
+       REAL(mytype), INTENT (IN) :: G11,G12,G13,G22,G23,G33
+       REAL(mytype), INTENT (IN) :: x1,x2,x3
+       REAL(mytype), dimension(3,3), INTENT (OUT) :: v
+       REAL(mytype) ::d,d1,d2,d3
+       REAL(mytype), dimension(3):: x
+       real(mytype),parameter::zero=0.0
+       integer i,j
+       REAL(mytype) ::B11,B22,B33
+       x(1) = x1;x(2)=x2;x(3)=x3
+       do i=1,3
+			B11 = G11-x(i)
+			B22 = G22-x(i)
+			B33 = G33-x(i)
+			v(3,i)=1.0
+			v(2,i)=(G13*G12/B11-G23)/(B22-G12*G12/B11)
+			v(1,i)=(-G12*v(2,i)-G13)/B11
+			v(:,i) = v(:,i) / sqrt(v(1,i)**2.0 + v(2,i)**2.0 + v(3,i)**2.0)
+		enddo
+		return
+end subroutine eigvec
+       
+		
   
   
  subroutine eig_sym_smith(G11,G12,G13,G22,G23,G33,x1,x2,x3)
