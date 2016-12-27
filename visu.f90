f8827e8fd6da9ec81c85edbe1ff8ebfc8ef3c794
alon <alon@manor>
2016-12-27 20:37:52 +0200

    LES visualization- nu & SGS TKE

--- visu.f90
+++ visu.f90
@@ -34,7 +34,7 @@
 !
 subroutine VISU_INSTA (ux1,uy1,uz1,phi1,ta1,tb1,tc1,td1,te1,tf1,tg1,th1,ti1,di1,&
      ta2,tb2,tc2,td2,te2,tf2,tg2,th2,ti2,tj2,di2,&
-     ta3,tb3,tc3,td3,te3,tf3,tg3,th3,ti3,di3,phG,uvisu)
+     ta3,tb3,tc3,td3,te3,tf3,tg3,th3,ti3,di3,phG,uvisu,k_sgs1,xnu_sgs1)
 !
 !############################################################################
 
@@ -144,6 +144,29 @@ call fine_to_coarseV(1,phi1,uvisu)
 !   call decomp_2d_write_one(nx_global,ny_global,nz_global,&
 !        1,phi1,filename)
 endif
+
+!############################################################################
+!visualization for LES
+!############################################################################
+! eddy viscousity
+if ((iles.eq.1).or.(iles.eq.2)) then
+	uvisu=0.
+	call fine_to_coarseV(1,xnu_sgs1,uvisu)
+997 format('nu_sgs',I3.3)
+	   write(filename, 997) itime/imodulo
+	   call decomp_2d_write_one(1,uvisu,filename,2)
+endif
+
+! SGS TKE
+if ((iles.eq.4).or.(iles.eq.5)) then
+	uvisu=0.
+	call fine_to_coarseV(1,k_sgs1,uvisu)
+998 format('tke_sgs',I3.3)
+	   write(filename, 998) itime/imodulo
+	   call decomp_2d_write_one(1,uvisu,filename,2)
+endif
+
+
 !############################################################################
 
 !############################################################################
